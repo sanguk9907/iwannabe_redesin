94b35aebb6c5247de2bfe3abf471579332206f77
sanguk9907 <tkddnr6079@naver.com>
2022-02-04 19:06:52 +0900

    코딩 2차 섹션3 배너 작업중

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        // direction: 'vertical',
+        loop: true,
+        speed: 1000,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction : false,
+        }
+        
+      });
+
+      $(".kakao-banner i").click(function(){
+          $(".kakao-banner").addClass("active")
+      });
+
+
+
+});
