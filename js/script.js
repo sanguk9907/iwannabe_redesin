21e86b1488c74b0ac5aba72fc3e3e3f1ed088bb1
sanguk9907 <tkddnr6079@naver.com>
2022-02-16 18:50:42 +0900

    메인페이지 끝

--- js/script.js
+++ js/script.js
@@ -31,4 +31,40 @@ $(document).ready(function(){
       AOS.init();
 
 
+
+      $(".top-bar-wrap > .top-bar > .top-box > .ham-icon").click(function(){
+        $(".gnb-wrap").addClass("active");
+      });
+
+      $(".gnb-wrap > .gnb-bg").click(function(){
+        $(".gnb-wrap").removeClass("active");
+      })
+      
+      $(".gnb-wrap > .gnb-box > .left > li").mouseover(function(){
+
+        var indexNum = $(this).index()
+
+        $(".gnb-wrap > .gnb-box > .left > li").removeClass("active");
+        $(this).addClass("active");
+        $(".gnb-wrap > .gnb-box > .right").eq(indexNum).siblings().removeClass("active");
+        $(".gnb-wrap > .gnb-box > .right").eq(indexNum).addClass("active");
+      });
+
+      $(".gnb-wrap > .gnb-box > i").click(function(){
+        $(".gnb-wrap").removeClass("active");
+      });
+
+      $(".top-bar-wrap > .top-bar > .top-box > .icon-box > a > .search-icon").click(function(){
+        $(".top-bar-wrap > .top-bar > .top-box > .icon-box > .search-box").addClass("active");
+        $(this).addClass("active");
+        $(".top-bar-wrap > .top-bar > .top-box > .icon-box > .search-box > .fa-times").addClass("active");
+      });
+
+      $(".top-bar-wrap > .top-bar > .top-box > .icon-box > .search-box > .fa-times").click(function(){
+        $(".top-bar-wrap > .top-bar > .top-box > .icon-box > .search-box").removeClass("active");
+        $(this).removeClass("active");
+        $(".top-bar-wrap > .top-bar > .top-box > .icon-box > a > .search-icon").removeClass("active");
+      });
+
+
 });
